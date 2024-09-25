3d60a5ca1c47dc53cdc79fc027058a4b67844721
marl0vv <23glad02@mail.ru>
2024-09-26 09:33:18 +1000

    Add POST method and tests for it

--- src/test/java/com/GlaMik/cashcard/CashCardJsonTest.java
+++ src/test/java/com/GlaMik/cashcard/CashCardJsonTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
+
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 
 @JsonTest
 class CashCardJsonTest {
