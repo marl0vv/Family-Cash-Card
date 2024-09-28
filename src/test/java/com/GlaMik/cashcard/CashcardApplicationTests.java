a6c01efea4e79c432287b8df11b9c55483649536
marl0vv <23glad02@mail.ru>
2024-09-28 21:55:03 +1000

    Add tests for array of Cashcards
    Okay, actually it was supposed to be a part of a big commit where implemented GET query to get list of cashcards with the help of pagination. But I am a little tired so I'll do it in a next commit tommorow.

--- src/test/java/com/GlaMik/cashcard/CashcardApplicationTests.java
+++ src/test/java/com/GlaMik/cashcard/CashcardApplicationTests.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.net.URI;
 
@@ -15,6 +16,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest(webEnvironment =
         SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class CashcardApplicationTests {
     @Autowired
     TestRestTemplate restTemplate;
